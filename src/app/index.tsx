d3498dc14c64e56a3ba8ee7f94823ec468434c3d
Shreya pal <shreyapal7985@gmail.com>
2026-05-31 12:56:43 +0530

    creating and displaying the data on UI

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,34 @@
-import { Text, View, StyleSheet } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  const todolist=[{
+    id:1,
+    title:"todo 1",
+    isdone:'false'
+  },
+{
+  id:1,
+  title:'todo 2',
+  isdone:'false'
+},
+{
+id:3,
+title: 'todo 3',
+isdone:'false'},
+{
+  id:4,
+  title:'todo 4',
+  isdone:"false"
+}]
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <Text>hello to do list application.</Text>
+      <FlatList
+      data={todolist}
+      keyExtractor={(item)=>item.id.toString()}
+      renderItem={({item})=><View><Text>{item.title}</Text></View>}
+      />
+      
     </View>
   );
 }
